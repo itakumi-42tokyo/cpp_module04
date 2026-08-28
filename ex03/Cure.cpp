4677d388e3436b59ed35aa9905ea21b8d76a414a
Igarashi Takumi <itakumi@c5r2s10.42tokyo.jp>
2026-08-28 15:12:33 +0900

    chore: change ' in ex03 Cure::clone func

--- ex03/Cure.cpp
+++ ex03/Cure.cpp
@@ -25,5 +25,5 @@ Cure::~Cure() {
 AMateria* Cure::clone() const { return new Cure(); }
 
 void Cure::use(ICharacter& target) {
-  std::cout << "* heals " << target.getName() << "’s wounds *" << std::endl;
+  std::cout << "* heals " << target.getName() << "'s wounds *" << std::endl;
 }
